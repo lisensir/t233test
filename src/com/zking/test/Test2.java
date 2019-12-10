47d49e6efbf97f9ff834be87a901e5fcbe9e4eac
lisen <lisensir@qq.com>
2019-12-10 19:58:50 +0800

    临时提交

---
+++ src/com/zking/test/Test2.java
@@ -0,0 +1,11 @@
+package com.zking.test;
+
+/**
+ * @author Administrator
+ * @create 2019-12-1019:56
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test 2");
+    }
+}
